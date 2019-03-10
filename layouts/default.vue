936cf3d646e1610ca159ba95a0397771410a8328
pontakornth <pontakorn61@gmail.com>
2019-03-10 21:06:02 +0700

    Remove navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,26 +25,7 @@
       </div>
     </nav>
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
+    <section class="main-content columns is-centered">
       <div class="container column is-10">
         <nuxt />
       </div>
@@ -53,22 +34,5 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
-  }
-}
+export default {}
 </script>
