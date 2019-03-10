b9426667b60e6949be702596919d0fc042195e63
pontakornth <pontakorn61@gmail.com>
2019-03-10 21:06:13 +0700

    Add prototype about

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="section">
+    <div class="content">
+      <p>Lorem ipsum dola mano ipsark koangff</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
