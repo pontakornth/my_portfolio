c6fdacc8d3a238f622abf153be5746e93c0af527
pontakornth <pontakorn61@gmail.com>
2019-03-11 17:29:39 +0700

    Add link button component

---
+++ components/LinkButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <nuxt-link :to="to" class="button">
+    <slot />
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'LinkButton',
+  props: {
+    to: {
+      default: '/',
+      type: String
+    }
+  }
+}
+</script>
