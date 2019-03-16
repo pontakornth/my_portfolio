70b595e73aae328af90b16ec1ed89110cfc49be1
pontakornth <pontakorn61@gmail.com>
2019-03-16 21:07:27 +0700

    Fix broken link

--- components/Contact.vue
+++ components/Contact.vue
@@ -9,7 +9,7 @@
           <ul class="fa-ul">
             <li><b-icon class="fa-li" pack="fab" icon="line" />most1562</li>
             <li><b-icon class="fa-li" pack="fab" icon="facebook" /><a target="_blank" href="https://www.facebook.com/most.terra">Pontakorn Paesaeng</a></li>
-            <li><b-icon class="fa-li" pack="fab" icon="github" /><a target="_blank" href="github.com/pontakornth">pontakornth</a></li>
+            <li><b-icon class="fa-li" pack="fab" icon="github" /><a target="_blank" href="https://github.com/pontakornth">pontakornth</a></li>
           </ul>
         </div>
       </div>
