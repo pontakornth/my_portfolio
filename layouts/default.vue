6367a3cb34c34ba824e4f1519a588aee3ffc03f9
pontakornth <pontakorn61@gmail.com>
2019-03-16 18:16:17 +0700

    Remove navigation bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,5 @@
 <template>
   <div>
-    <nav
-      class="navbar header is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <h1>
-            Pontakorn Paesaeng
-          </h1>
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
     <nuxt />
   </div>
 </template>
