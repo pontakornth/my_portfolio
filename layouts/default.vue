3e6cbfa1e71200fb1e4abd2af505176b319da071
pontakornth <pontakorn61@gmail.com>
2019-03-16 19:12:32 +0700

    Remove unnecessary page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,37 @@
 <template>
   <div>
+    <nav
+      class="navbar header is-info"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          <h1>
+            Pontakorn Paesaeng
+          </h1>
+        </a>
+      </div>
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <a @click="cheat" class="navbar-item"> กดสูตรตรงนี้ </a>
+        </div>
+      </div>
+    </nav>
     <nuxt />
   </div>
 </template>
 
 <script>
-export default {}
+import { mapActions } from 'vuex'
+export default {
+  methods: {
+    ...mapActions({
+      cheat: 'game/cheat'
+    })
+  }
+}
 </script>
